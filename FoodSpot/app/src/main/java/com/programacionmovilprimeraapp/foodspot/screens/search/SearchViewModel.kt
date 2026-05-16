fb55a521364e7ff06aaa03410385fb3e97ae9670
SamuelAlejandroPerezHernandez <00052424@uca.edu.sv>
2026-05-16 00:46:03 -0600

    feat: implementacion de el viewModel de la pantalla de search

--- FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/screens/search/SearchViewModel.kt
+++ FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/screens/search/SearchViewModel.kt
@@ -1,2 +1,43 @@
 package com.programacionmovilprimeraapp.foodspot.screens.search
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.programacionmovilprimeraapp.foodspot.data.RestaurantRepository
+import com.programacionmovilprimeraapp.foodspot.data.RestaurantRepositoryImplement
+import com.programacionmovilprimeraapp.foodspot.model.Restaurant
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SearchViewModel: ViewModel(){
+
+    val repository: RestaurantRepository = RestaurantRepositoryImplement()
+
+    val _restaurant = MutableStateFlow<List<Restaurant>>(emptyList())
+
+    val _filteredList = MutableStateFlow<List<Restaurant>>(emptyList())
+    val filteredName = _filteredList.asStateFlow()
+
+    init{
+        loadRestaurants("")
+    }
+
+    fun loadRestaurants(filteredName: String){
+        viewModelScope.launch{
+            _restaurant.value = repository.getRestaurants()
+
+            val filteredRestaurants = _restaurant.value.filter{
+                restaurants ->
+                restaurants.name.contains(filteredName, ignoreCase = true)
+                        ||
+                restaurants.menu.any{
+                    dish ->
+                    dish.name.contains(filteredName, ignoreCase = true)
+                }
+            }
+
+            _filteredList.value = filteredRestaurants
+        }
+    }
+
+}
