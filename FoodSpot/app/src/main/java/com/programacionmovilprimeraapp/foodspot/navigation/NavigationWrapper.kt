d2e7c338a27552d23cce841a40fe84fcf0a25f7d
SamuelAlejandroPerezHernandez <00052424@uca.edu.sv>
2026-05-16 13:07:03 -0600

    implementacion de la navegacion y las rutas

--- FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/navigation/NavigationWrapper.kt
+++ FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/navigation/NavigationWrapper.kt
@@ -1,2 +1,60 @@
 package com.programacionmovilprimeraapp.foodspot.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import com.programacionmovilprimeraapp.foodspot.screens.detail.Detail
+import com.programacionmovilprimeraapp.foodspot.screens.home.Home
+import com.programacionmovilprimeraapp.foodspot.screens.search.Search
+
+@Composable
+fun NavigationWrapper(){
+    val backStack = rememberNavBackStack(Routes.home)
+
+    NavDisplay(
+        backStack = backStack,
+
+        onBack = {
+            backStack.removeLastOrNull()
+        },
+
+        entryProvider = entryProvider{
+            entry<Routes.home>{
+                Home(
+                    goToDetail = {
+                        restaurantId ->
+                        backStack.add(Routes.detail(restaurantId))
+                    },
+
+                    goToSearch = {
+                        backStack.add(Routes.search)
+                    }
+                )
+            }
+
+            entry<Routes.detail>{
+                Detail(
+                    restaurantId = it.restaurantId,
+
+                    goBack = {
+                        backStack.removeLastOrNull()
+                    }
+                )
+            }
+
+            entry<Routes.search>{
+                Search(
+                    goBack = {
+                        backStack.removeLastOrNull()
+                    },
+
+                    goToDetail = {
+                        restaurantId ->
+                        backStack.add(Routes.detail(restaurantId))
+                    }
+                )
+            }
+        }
+    )
+}
--- FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/navigation/Routes.kt
+++ FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/navigation/Routes.kt
@@ -1,2 +1,16 @@
 package com.programacionmovilprimeraapp.foodspot.navigation
 
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+sealed class Routes: NavKey{
+
+    @Serializable
+    data object home: Routes()
+
+    @Serializable
+    data class detail(val restaurantId: Int): Routes()
+
+    @Serializable
+    data object search: Routes()
+}
