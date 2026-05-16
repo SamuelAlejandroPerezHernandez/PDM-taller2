33c6c107fe827248a122754950068bf57cc13f50
SamuelAlejandroPerezHernandez <00052424@uca.edu.sv>
2026-05-16 10:13:38 -0600

    feat: implementacion de la screen de la pantalla de search implementando el viewModel

--- FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/screens/search/SearchScreen.kt
+++ FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/screens/search/SearchScreen.kt
@@ -1,2 +1,154 @@
 package com.programacionmovilprimeraapp.foodspot.screens.search
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.programacionmovilprimeraapp.foodspot.model.Restaurant
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun Search(
+    goBack: () -> Unit
+){
+    val viewModel: SearchViewModel = viewModel()
+
+    val selectedRestaurant by
+    viewModel.filteredName.collectAsState()
+
+    var searchText by rememberSaveable{mutableStateOf("")}
+
+    val changeTextFild: (String) -> Unit = {
+        it -> searchText = it
+    }
+
+    val loadRestaurantList = {
+        viewModel.loadRestaurants(searchText)
+    }
+
+    Scaffold(
+        topBar = { SearchTopAppBar(goBack) },
+        bottomBar = { SearchBottomBar() }
+    ){
+        innerPadding ->
+        SearchContent(innerPadding, searchText, changeTextFild, selectedRestaurant, loadRestaurantList)
+    }
+}
+
+@Composable
+fun SearchContent(
+    padding: PaddingValues,
+    searchText: String,
+    changeTextFild: (String) -> Unit,
+    selectedRestaurant: List<Restaurant>,
+    loadRestaurantList: () -> Unit
+){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        item{
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+            ){
+                TextField(
+                    value = searchText,
+                    onValueChange = { changeTextFild(it) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    placeholder = { Text("Buscar restaurante...") }
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Button(
+                    onClick = { loadRestaurantList() },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ){
+                    Text(text = "Buscar")
+                }
+            }
+        }
+
+        items(selectedRestaurant){
+            restaurante ->
+            Card(
+                onClick = {},
+                modifier = Modifier
+                    .width(250.dp)
+                    .padding(horizontal = 12.dp, vertical = 8.dp)
+                    .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Box(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp)
+                    ){
+                        AsyncImage(
+                            model = restaurante.imageUrl,
+                            contentDescription = "restaurant image",
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+
+                    Column(
+                        modifier = Modifier.padding(12.dp)
+                    ) {
+                        Text(
+                            text = restaurante.name,
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
--- FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/screens/search/components.kt
+++ FoodSpot/app/src/main/java/com/programacionmovilprimeraapp/foodspot/screens/search/components.kt
@@ -1,2 +1,111 @@
 package com.programacionmovilprimeraapp.foodspot.screens.search
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBackIosNew
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchTopAppBar(
+    goBack: () -> Unit
+){
+    TopAppBar(
+        title = {},
+
+        navigationIcon = {
+            IconButton(
+                onClick = { goBack() },
+                modifier = Modifier
+                    .size(40.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Rounded.ArrowBackIosNew,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+        },
+
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Black
+        )
+    )
+}
+
+@Composable
+fun SearchBottomBar(){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+            .windowInsetsPadding(WindowInsets.navigationBars)
+            .padding(top = 25.dp, bottom = 40.dp)
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(30.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Rounded.Home,
+                    contentDescription = "home",
+                    tint = Color.White,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(30.dp)
+                    .clip(CircleShape)
+                    .background(Color.White)
+            ){
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = "home",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+
+
+        }
+    }
+}
